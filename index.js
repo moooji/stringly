916a222446ab163221b1e0b0231549702417d5ff
moooji <steffen.straetz@gmail.com>
2016-06-05 13:02:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+function parseTitle(post) {
+  if (validation.isString(post.title)) {
+    post.title = post.title.replace(/^\s+|\s+$/g, '');
+    post.title = post.title.toLowerCase();
+    post.title = _.trim(post.title);
+    post.title = _.capitalize(post.title);
+  }
+
+  return post;
+}
+*/
